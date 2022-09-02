85d0d20af8077313f83a350d4cac269bb2ff042e
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-02 08:07:15 +0300

    1.2: course info, step2

---
+++ part1/course-info/src/components/Part.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Part = (props) => {
+    return(
+        <p>
+                {props.part} {props.exercises}
+        </p>
+    );
+}
+
+export default Part;
