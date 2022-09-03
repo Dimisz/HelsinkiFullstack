817cb38af3a06d01e905cd0bfd6b310b3fcc1b0f
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-03 11:58:25 +0300

    1.9: unicafe step4

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,23 +1,30 @@
 const Statistics = ({ good, neutral, bad }) => {
     const total = good + neutral + bad;
-    let average = 0;
-    let positive = 0;
+    
+    const average = (good - bad) / total;
+    const positive = (good / total) * 100;
+   
     if(total > 0){
-        average = (good - bad) / total;
-        positive = (good / total) * 100;
+        return (
+            <section>
+                <h1>statistics</h1>
+                <p>good {good}</p>
+                <p>neutral {neutral}</p>
+                <p>bad {bad}</p>
+                <p>all {total}</p>
+                <p>average {average}</p>
+                <p>positive {positive} %</p>
+            </section>
+        )
     }
-
-    return (
-        <section>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {total}</p>
-            <p>average {average}</p>
-            <p>positive {positive} %</p>
-        </section>
-    )
+    else {
+        return (
+            <section>
+                <h1>statistics</h1>
+                <p>No feedback given</p>
+            </section>
+        )
+    }
 }
 
 export default Statistics;
