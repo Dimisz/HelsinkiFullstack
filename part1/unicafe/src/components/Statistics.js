1fab4d652312b50b3e8525fa7e0da3969419fc9e
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-03 12:10:04 +0300

    1.11*: unicafe step6

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -4,18 +4,22 @@ const Statistics = ({ good, neutral, bad }) => {
     const total = good + neutral + bad;
     
     const average = (good - bad) / total;
-    const positive = (good / total) * 100;
+    const positive = ((good / total) * 100) + " %";
    
     if(total > 0){
         return (
             <section>
                 <h1>statistics</h1>
-                <StatisticLine text="good" value={good} />
-                <StatisticLine text="neutral" value={neutral} />
-                <StatisticLine text="bad" value={bad} />
-                <StatisticLine text="total" value={total} />
-                <StatisticLine text="average" value={average} />
-                <StatisticLine text="positive" value={(positive+' %')} />
+                <table>
+                    <tbody>
+                        <StatisticLine text="good" value={good} />
+                        <StatisticLine text="neutral" value={neutral} />
+                        <StatisticLine text="bad" value={bad} />
+                        <StatisticLine text="total" value={total} />
+                        <StatisticLine text="average" value={average} />
+                        <StatisticLine text="positive" value={positive} />
+                    </tbody>
+                </table>
             </section>
         )
     }
