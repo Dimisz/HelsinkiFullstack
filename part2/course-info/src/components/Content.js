63c988ca57283f47abce0d27a8268bc56aab155d
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-04 19:13:09 +0300

    2.1: course info step6

---
+++ part2/course-info/src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Part from './Part';
+const Content = ({parts}) => {
+    // const arr = props;
+    // console.log(typeof(arr));
+    // console.log(arr);
+    return(
+        <>
+            {
+                parts.map((part) => (
+                    <Part key={part.id} part={part.name} exercises={part.exercises} />
+                ))
+                
+            }
+        </>
+    );
+}
+
+export default Content;
