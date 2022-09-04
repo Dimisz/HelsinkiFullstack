daf95342d4769c0f6cf718c521af7e144e3c2a68
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-04 19:20:04 +0300

    2.2: Course information step7

--- part2/course-info/src/components/Content.js
+++ part2/course-info/src/components/Content.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Part from './Part';
+
 const Content = ({parts}) => {
     // const arr = props;
     // console.log(typeof(arr));
@@ -12,7 +13,6 @@ const Content = ({parts}) => {
                 parts.map((part) => (
                     <Part key={part.id} part={part.name} exercises={part.exercises} />
                 ))
-                
             }
         </>
     );
--- part2/course-info/src/components/Course.js
+++ part2/course-info/src/components/Course.js
@@ -1,11 +1,17 @@
 import Header from "./Header";
 import Content from "./Content";
-
+import Total from "./Total";
 const Course = ({ course }) => {
+    let totalExercises = 0;
+    for(let i = 0; i < course.parts.length; i++){
+        totalExercises += course.parts[i].exercises;
+    }
+    
     return(
         <>
             <Header courseName={course.name} />
             <Content parts={course.parts} />
+            <Total total={totalExercises} />
         </>
     )
 }
