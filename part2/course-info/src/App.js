8677aabbb4f60ee2baf46898c8fd8dde6dfa0e7f
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-05 20:10:18 +0300

    2.5: separate module

--- part2/course-info/src/App.js
+++ part2/course-info/src/App.js
@@ -1,27 +1,6 @@
 import Course from "./components/Course";
 
 const App = () => {
-  // const course = {
-  //   id: 1,
-  //   name: 'Half Stack application development',
-  //   parts: [
-  //     {
-  //       name: 'Fundamentals of React',
-  //       exercises: 10,
-  //       id: 1
-  //     },
-  //     {
-  //       name: 'Using props to pass data',
-  //       exercises: 7,
-  //       id: 2
-  //     },
-  //     {
-  //       name: 'State of a component',
-  //       exercises: 14,
-  //       id: 3
-  //     }
-  //   ]
-  // }
 
   const courses = [
     {
@@ -73,7 +52,6 @@ const App = () => {
       <Course key={course.id} course={course} />
     ))
   )
-  
   
 }
 
